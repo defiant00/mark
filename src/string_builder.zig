965683398bb327a35d78fc5cef4ee5f9eff23bfd
defiant00 <defiant00@gmail.com>
2023-05-05 11:38:26 -0700

    StringBuilder cleanup and write

--- src/string_builder.zig
+++ src/string_builder.zig
@@ -5,25 +5,25 @@ test {
     std.testing.refAllDecls(@This());
 }
 
-pub const StringBuilderItem = struct {
-    value: []const u8,
-    next: ?*StringBuilderItem,
-
-    fn init(alloc: Allocator, val: []const u8) !*StringBuilderItem {
-        const new = try alloc.create(StringBuilderItem);
-        new.* = .{
-            .value = val,
-            .next = null,
-        };
-        return new;
-    }
-};
-
 pub const StringBuilder = struct {
+    pub const Item = struct {
+        value: []const u8,
+        next: ?*Item,
+
+        fn init(alloc: Allocator, val: []const u8) !*Item {
+            const new = try alloc.create(Item);
+            new.* = .{
+                .value = val,
+                .next = null,
+            };
+            return new;
+        }
+    };
+
     allocator: Allocator,
 
-    first: ?*StringBuilderItem,
-    last: ?*StringBuilderItem,
+    first: ?*Item,
+    last: ?*Item,
 
     pub fn init(alloc: Allocator) StringBuilder {
         return .{
@@ -52,8 +52,8 @@ pub const StringBuilder = struct {
         return res;
     }
 
-    pub fn append(self: *StringBuilder, val: []const u8) !*StringBuilderItem {
-        const new = try StringBuilderItem.init(self.allocator, val);
+    pub fn append(self: *StringBuilder, val: []const u8) !*Item {
+        const new = try Item.init(self.allocator, val);
         if (self.last) |l| {
             l.next = new;
         } else {
@@ -63,7 +63,7 @@ pub const StringBuilder = struct {
         return new;
     }
 
-    pub fn appendLine(self: *StringBuilder, val: []const u8) !*StringBuilderItem {
+    pub fn appendLine(self: *StringBuilder, val: []const u8) !*Item {
         const new = try self.append(val);
         _ = try self.append("\n");
         return new;
@@ -83,10 +83,11 @@ pub const StringBuilder = struct {
         return str;
     }
 
-    test "length when empty" {
-        var sb = StringBuilder.init(std.testing.allocator);
-        defer sb.deinit();
-        std.debug.assert(sb.length() == 0);
+    pub fn write(self: *StringBuilder, writer: anytype) !void {
+        var current = self.first;
+        while (current) |cur| : (current = cur.next) {
+            try writer.writeAll(cur.value);
+        }
     }
 
     test "length" {
@@ -98,6 +99,24 @@ pub const StringBuilder = struct {
         _ = try sb.append("6789");
 
         std.debug.assert(sb.length() == 9);
+    }
+
+    test "length when empty" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+        std.debug.assert(sb.length() == 0);
+    }
+
+    test "length with empties" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.append("");
+        _ = try sb.append("123");
+        var item = try sb.append("x");
+        item.value = "";
+
+        std.debug.assert(sb.length() == 3);
     }
 
     test "length with newlines" {
@@ -124,16 +143,6 @@ pub const StringBuilder = struct {
         std.debug.assert(sb.length() == 7);
     }
 
-    test "toString when empty" {
-        var sb = StringBuilder.init(std.testing.allocator);
-        defer sb.deinit();
-
-        const str = try sb.toString();
-        defer sb.allocator.free(str);
-
-        std.debug.assert(std.mem.eql(u8, str, ""));
-    }
-
     test "toString" {
         var sb = StringBuilder.init(std.testing.allocator);
         defer sb.deinit();
@@ -146,6 +155,31 @@ pub const StringBuilder = struct {
         defer sb.allocator.free(str);
 
         std.debug.assert(std.mem.eql(u8, str, "123456789"));
+    }
+
+    test "toString when empty" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        const str = try sb.toString();
+        defer sb.allocator.free(str);
+
+        std.debug.assert(std.mem.eql(u8, str, ""));
+    }
+
+    test "toString with empties" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.append("");
+        _ = try sb.append("123");
+        var item = try sb.append("x");
+        item.value = "";
+
+        const str = try sb.toString();
+        defer sb.allocator.free(str);
+
+        std.debug.assert(std.mem.eql(u8, str, "123"));
     }
 
     test "toString with newlines" {
@@ -176,5 +210,79 @@ pub const StringBuilder = struct {
         defer sb.allocator.free(str);
 
         std.debug.assert(std.mem.eql(u8, str, "123x789"));
+    }
+
+    test "write" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.append("123");
+        _ = try sb.append("45");
+        _ = try sb.append("6789");
+
+        var list = std.ArrayList(u8).init(sb.allocator);
+        defer list.deinit();
+        try sb.write(list.writer());
+
+        std.debug.assert(std.mem.eql(u8, list.items, "123456789"));
+    }
+
+    test "write when empty" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        var list = std.ArrayList(u8).init(sb.allocator);
+        defer list.deinit();
+        try sb.write(list.writer());
+
+        std.debug.assert(std.mem.eql(u8, list.items, ""));
+    }
+
+    test "write with empties" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.append("");
+        _ = try sb.append("123");
+        var item = try sb.append("x");
+        item.value = "";
+
+        var list = std.ArrayList(u8).init(sb.allocator);
+        defer list.deinit();
+        try sb.write(list.writer());
+
+        std.debug.assert(std.mem.eql(u8, list.items, "123"));
+    }
+
+    test "write with newlines" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.appendLine("123");
+        _ = try sb.appendLine("45");
+        _ = try sb.append("6789");
+
+        var list = std.ArrayList(u8).init(sb.allocator);
+        defer list.deinit();
+        try sb.write(list.writer());
+
+        std.debug.assert(std.mem.eql(u8, list.items, "123\n45\n6789"));
+    }
+
+    test "write with replaced item" {
+        var sb = StringBuilder.init(std.testing.allocator);
+        defer sb.deinit();
+
+        _ = try sb.append("123");
+        var item = try sb.append("456");
+        _ = try sb.append("789");
+
+        item.value = "x";
+
+        var list = std.ArrayList(u8).init(sb.allocator);
+        defer list.deinit();
+        try sb.write(list.writer());
+
+        std.debug.assert(std.mem.eql(u8, list.items, "123x789"));
     }
 };
